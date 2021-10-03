ca54ad19ad3c89594c68350dd70ac5b2da7beacd
Kevin Grace <kgrace90@yahoo.com>
2021-10-03 16:53:33 -0400

    update to connections and html

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -37,7 +37,7 @@ router.get('/', withAuth, (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+      res.render('/index.html', { posts, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
